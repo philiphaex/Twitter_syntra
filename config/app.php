000f78ed59fe51720afd2bb350a6a3d3856b895f
philiphaex <philip_haex@hotmail.com>
2017-05-10 21:52:59 +0200

    finished

--- config/app.php
+++ config/app.php
@@ -38,7 +38,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', true),
+    'debug' => env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -103,7 +103,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY','abcdefghijklmnopqrstuvwxyz123456'),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
